6a1ffbed68b14f402c6cb5fea0677e0157f45735
David Ribeiro <dcaliari@gmail.com>
2022-04-09 07:41:37 -0300

    Incluindo migrations

--- 03_RestWithASPNETUdemy_Calculator_Post/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
+++ 03_RestWithASPNETUdemy_Calculator_Post/RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -2,8 +2,14 @@ using Microsoft.EntityFrameworkCore;
 using RestWithASPNETUdemy.Business.Implementations;
 using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Repository.Implementations;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+
 
 // Add services to the container.
 
@@ -21,6 +27,40 @@ builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
 builder.Services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
 
 var app = builder.Build();
+
+
+if(app.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+    
+    using(var scope = app.Services.CreateScope())
+    {
+        var evolveDbContext = scope.ServiceProvider.GetRequiredService<MySQLContext>();
+        evolveDbContext.Database.Migrate();
+    }
+    
+    
+}
+
+void MigrateDatabase(string connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnector.MySqlConnection(connection);
+        var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true
+        };
+        evolve.Migrate();
+    }catch(Exception ex)
+    {
+        Log.Error("Database migration failde", ex);
+        throw;
+    }
+}
+
+
 
 // Configure the HTTP request pipeline.
 
---
+++ 03_RestWithASPNETUdemy_Calculator_Post/RestWithASPNETUdemy/RestWithASPNETUdemy/db/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anciano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(3, 'Portbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(4, 'Kentuchy - USA', 'Mohamed Ali', 'Male', 'Gandhi'),
+	(5, 'Belo Horizonte', 'David', 'Male', 'Ribeiro');
